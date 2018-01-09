e41cf83ef8b9ea7d6b9ca2637ac733760f04ca7b
Jie Zheng <jzhengcse@gmail.com>
2018-01-09 10:34:26 -0500

    finished stock components

---
+++ myAssistant/src/components/Stock.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+var alpha = require('alphavantage')({ key: 'qweqweqwe' });
+const Stock = (props) => (
+  <div>
+    {/*<label>Pick random media </label>*/}
+    {/*alpha.data.intraday('msft'.then(data => {*/}
+      {/*console.log(data)*/}
+    {/*});*/}
+    {/*<p>Result: <span id="item"></span></p>*/}
+    <h1>Hello</h1>
+  </div>
+)
+export default Stock
